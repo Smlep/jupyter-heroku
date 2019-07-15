f71be870f0d6fdf908d9b37e4148f161a5d8c7ff
Sam Courtemanche <sam.courtemanche@epita.fr>
2019-07-15 02:56:53 +0200

    setup: move dependencies download before notebook start

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+# Download custom packages for python
+pip install -r notebooks-requirements.txt
+
 # Launch notebook
 jupyter notebook --port $PORT --ip=0.0.0.0 \
   --no-browser src
 
-# Download custom packages for python
-
-pip install -r notebooks-requirements.txt
