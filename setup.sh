45e23eb77097f6ea3cca99cf62f1e906e0054071
Sam Courtemanche <sam.courtemanche@epita.fr>
2019-07-15 02:54:42 +0200

    requirements: add numpy, pandas and matplotlib

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo "Port is $PORT"
+# Launch notebook
 jupyter notebook --port $PORT --ip=0.0.0.0 \
   --no-browser src
+
+# Download custom packages for python
+
+pip install -r notebooks-requirements.txt
